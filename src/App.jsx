560f4a900eacade5ad73609a7ef505897f859b43
Cameron Griffith <camgriff@cs.du.edu>
2019-11-15 05:29:24 -0700

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 		<div className="container">
 		<div className="game">
 			<h1>Solve the Puzzle!</h1>
-			<Game/>
+			<Game id="Game"/>
 			<h3>This is a tile puzzle game. The goal of the game is to place all the tiles into numerical order by sliding each tile only using the empty space.</h3>
 		</div>
 		<div className="bottom">&copy; 2019 Cameron Griffith. All Rights Reserved.</div>
